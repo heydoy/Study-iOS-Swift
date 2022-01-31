96b037d19a466cd72d914d9a507e5c846e597249
Kim Doy <heydoyhey@gmail.com>
2022-02-01 06:56:12 +0900

    while, break, repeat-while statement 연습'

--- Playground/FlowControl.playground/Contents.swift
+++ Playground/FlowControl.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// --- While Loop
+// 특정 조건에 따라 동작을 반복시킴
+/*
+while 조건 {
+    코드블럭...
+}
+*/
+
+//var i = 0
+//while i < 10 {
+//    print(i)
+//    i += 1
+//}
+
+// break 문 사용
+print("---While")
+var i = 10
+while i < 10 {
+    print(i)
+//    if j==5 {
+//        break
+//    }
+    i += 1
+}
+
+// while-repeat 문 사용
+print("---Repeat")
+i = 10
+repeat {
+    print(i)
+    i += 1
+} while i < 10
