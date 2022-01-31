2315025a0e12c57284ae2eed487916c9da05b133
Kim Doy <heydoyhey@gmail.com>
2022-01-31 18:14:30 +0900

    commenting 연습

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
+/*
+ 여러줄 코멘트를 할 때
+ 사용하는 /**/
+ */
 
+// 한 줄 코멘트일 때 사용하는 //
 var greeting = "Hello, playground"
+
+//랜덤 숫자 (0~100 사이)
+let value = arc4random_uniform(100)
+
+//디버그 콘솔창에 출력하기
+print("-->\(value)")
+
