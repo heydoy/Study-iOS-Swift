94a471de7f6cc74c9be20b66fedf01a7b23ee70b
Kim Doy <heydoyhey@gmail.com>
2022-01-31 18:24:33 +0900

    Tuple 공부, 연습

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -1,15 +1,37 @@
 import UIKit
-/*
- 여러줄 코멘트를 할 때
- 사용하는 /**/
- */
 
-// 한 줄 코멘트일 때 사용하는 //
-var greeting = "Hello, playground"
+//// --- Comment
+///*
+// 여러줄 코멘트를 할 때
+// 사용하는 /**/
+// */
+//
+//// 한 줄 코멘트일 때 사용하는 //
+//var greeting = "Hello, playground"
+//
+////랜덤 숫자 (0~100 사이)
+//let value = arc4random_uniform(100)
+//
+////디버그 콘솔창에 출력하기
+//print("-->\(value)")
 
-//랜덤 숫자 (0~100 사이)
-let value = arc4random_uniform(100)
 
-//디버그 콘솔창에 출력하기
-print("-->\(value)")
+// --- Tuple 튜플
 
+let coordinates = (4, 6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+// 0, 1보다 명시적으로 표현하는 방법
+
+let coordinatesNamed = (x:2, y:3)
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+// 한줄로 가져올 수 있다.
+
+let (x3, y3) = coordinatesNamed
+x3
+y3
