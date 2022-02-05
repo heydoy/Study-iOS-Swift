80dec17f8c9731a1874b36974dd5d9c858dd4696
Kim Doy <heydoyhey@gmail.com>
2022-02-05 23:53:58 +0900

    Protocol : 프로토콜 실습

--- Playground/Structure-Challenge.playground/Contents.swift
+++ Playground/Structure-Challenge.playground/Contents.swift
@@ -1,15 +1,24 @@
 import UIKit
 
-// 도전과제
+// --- 도전과제
 // 1. 강의이름, 강사이름, 학생수를 가지는 Struct 만들기(Lecture)
 // 2. 강의 Array와 강사 이름을 받아서, 해당 강사의 강의 이름을 출력하는 함수 만들기
 // 3. 강의 3개 만들고 강사이름으로 강의 찾기
 
-struct Lecture {
+// --- 프로토콜 적용하기
+// CustomStringConvertible
+
+struct Lecture: CustomStringConvertible{
+    var description: String {
+        // 이 struct에 대한 description을 작성해서 리턴
+        return "Title: \(name), Instructor: \(instructor)"
+    }
+    
     let name : String
     let instructor : String
     let numberOfStudents : Int
 }
+
 
 func printTeacherLecture(lectures: [Lecture], instructor: String) {
 //    // --- 기존 함수
@@ -42,4 +51,5 @@ let lectures = [lec1, lec2, lec3]
 
 printTeacherLecture(lectures: lectures, instructor: "Janine")
 
+print(lec1)
 
