4c5e5488ba080f924d7dd7a73ff7731a7e6f9bd4
Kim Doy <heydoyhey@gmail.com>
2022-02-11 14:15:36 +0900

    Inheritance - 상속의 규칙 - Override - Uppercast, Downcast git commit -m Inheritance

--- Playground/Class-Inheritance.playground/Contents.swift
+++ Playground/Class-Inheritance.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// --- 상속의 개념
+
 struct Grade {
     var letter: Character
     var points: Double
@@ -38,4 +40,61 @@ student1.grades.append(history)
 
 student1.grades.count
 
-person1.grades.append(math) 
+// person1.grades.append(math)
+
+// --- 상속의 규칙과 실습
+
+// 학생인데 운동선수 , 사람 <-학생 <- 운동선수
+class StudentAthlete: Student {
+    var minimumTrainingTime: Int = 2
+    var trainedTime: Int = 0
+    
+    func train() {
+        trainedTime += 1
+    }
+}
+
+// 운동선수인데 축구선수, 사람 <- 학생 <- 운동선수 <- 축구선수
+class FootballPlayer: StudentAthlete {
+    var footballTeam = "FC Swift"
+    
+    // 상속받은 함수를 변경시킬 때 override 키워드를 사용
+    override func train() {
+        trainedTime += 2
+    }
+}
+
+// 인스턴스 만들기
+
+var athelete1 = StudentAthlete(firstName: "Junhwan", lastName: "Cha")
+
+var athelete2 = FootballPlayer(firstName: "Heungmin", lastName: "Son")
+
+athelete1.firstName
+athelete2.firstName
+
+athelete1.grades.append(math)
+athelete2.grades.append(math)
+
+athelete1.minimumTrainingTime
+athelete2.minimumTrainingTime
+
+athelete2.footballTeam
+//athelete1.footballTeam
+
+athelete1.train()
+athelete1.trainedTime
+athelete2.train()
+athelete2.trainedTime
+
+
+//uppercast
+athelete1 = athelete2 as StudentAthlete
+
+athelete1.train()
+athelete1.trainedTime
+
+//downcast
+if let son = athelete1 as? FootballPlayer {
+    print("--> \(son.footballTeam)")
+}
