62f23a57f94106c6e971e4f55aaee349e163b8f2
Kim Doy <heydoyhey@gmail.com>
2022-02-22 20:19:10 +0900

    MVVM 디자인패턴 적용: ViewModel과 View 적용

--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -30,10 +30,12 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
     
+    let viewModel = DetailViewModel()
+    
 //    var name: String?
 //    var bounty: Int?
     
-    var bountyInfo: BountyInfo?
+//    var bountyInfo: BountyInfo?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,11 +43,19 @@ class DetailViewController: UIViewController {
     }
     
     func updateUI() {
-        if let bountyInfo = self.bountyInfo {
-            imgView.image =  bountyInfo.image
-            nameLabel.text = bountyInfo.name
-            bountyLabel.text = "\(bountyInfo.bounty)"
-        }
+        if let bountyInfo = viewModel.bountyInfo {
+                    imgView.image =  bountyInfo.image
+                    nameLabel.text = bountyInfo.name
+                    bountyLabel.text = "\(bountyInfo.bounty)"
+                }
+        
+//        if let bountyInfo = self.bountyInfo {
+//            imgView.image =  bountyInfo.image
+//            nameLabel.text = bountyInfo.name
+//            bountyLabel.text = "\(bountyInfo.bounty)"
+//        }
+        
+        
 //        if let name = self.name, let bounty = self.bounty  {
 //            let img = UIImage(named: "\(name).jpg")
 //            imgView.image =  img
@@ -57,6 +67,16 @@ class DetailViewController: UIViewController {
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+}
 
+
+class DetailViewModel {
+    // 뷰모델은 모델을 가지고 있어야 한다.
+    var bountyInfo: BountyInfo?
+
+    // bountyInfo 내용을 업데이트하는 함수
+    func update(model : BountyInfo?) {
+        bountyInfo = model
+    }
+    
 }
